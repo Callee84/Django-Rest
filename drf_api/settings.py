8e914284641f3307d21119cf836413dc63b25807
Callee84 <carl.g.holm@gmail.com>
2022-12-20 16:46:39 +0000

    Add serializers

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -48,9 +48,9 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'rest_framework',
     'profiles',
-
-]
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
